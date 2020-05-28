6414426a364b3ad1a18b2c2c9ad0ab55058dbc2b
ericlin11354 <50276107+ericlin11354@users.noreply.github.com>
2020-05-28 16:58:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var wallId = 0;
 var wallSRC = ["front_wall.png", "right_wall.png", "back_wall.png", "left_wall.png"];
 var walls = ["front_wall", "right_wall", "back_wall", "left_wall"];
+var items= ["clock-key", "note", "picture", "gun"];
+var itemSRC= ["clock_key.png", "note.png", "picture.png", "gun.png"];
 
-var userItem = null;
+var userItems = [null, null];
 
 function changeRoom(dir) {
+    pauseAudio(wallId);
     document.getElementById(walls[wallId]).style.display = "none";
     if (dir=="right") {
         wallId = (wallId + 1) % 4;
@@ -15,6 +18,7 @@ function changeRoom(dir) {
             wallId = 3;
         }
     }
+    playAudio(wallId);
     document.getElementById("image-background").src = wallSRC[wallId];
     document.getElementById(walls[wallId]).style.display = "block";
 }
@@ -23,6 +27,22 @@ function showImg(item) {
     switch (item) {
         case 'safe':
             document.getElementById("image-safe").style.display = "block";
+            break;
+        case 'slot-1':
+            img = document.getElementById("image");
+            console.log(img.src);
+            if (img.getAttribute('src') != "slot.png")
+                img.src = "slot.png";
+            else
+                img.src = document.getElementById("image-slot-1").src;
+            break;
+        case 'slot-2':
+            img = document.getElementById("image");
+            console.log(img.src);
+            if (img.getAttribute('src') != "slot.png")
+                img.src = "slot.png";
+            else
+                img.src = document.getElementById("image-slot-2").src;
             break;
         default:
             document.getElementById("text").innerHTML = "Here's a "+item+"!" ;
@@ -36,10 +56,75 @@ function hideImg(item){
             document.getElementById("image-bookshelf-side").style.display = "block";
             break;
         case 'clock':
-            if (userItem != "clock-key")
-                document.getElementById("text").innerHTML = "The clock appears to be locked. Perhaps there is a key lying around."
-            else
+            if (userHasItem('clock-key')) {
                 document.getElementById("image-clock").style.display = "none";
+                loseItem('clock-key');
+            }
+            else
+                document.getElementById("text").innerHTML = "The clock appears to be locked. Perhaps there is a key lying around.";
+            break;
+        case 'clock-key':
+            pickUpItem(item);
+            document.getElementById("image-clock-key").style.display = "none";
+            break;
+        case 'picture':
+            pickUpItem(item);
+            document.getElementById("image-picture").style.display = "none";
+            break;
+    }
+}
+
+function userHasItem(item) {
+    for (i=0;i<userItems.length;i++) {
+        if (item == userItems[i])
+            return true
+    }
+    return false
+}
+
+function pickUpItem(item) {
+    for (i=0;i<userItems.length;i++) {
+        if (userItems[i] == null) {
+            userItems[i] = item;
+            listItems();
+            return;
+        }
+    }
+    console.log("Error", "Picked up too many items!");
+}
+
+function listItems() {
+    if (userItems[0] != null) {
+        document.getElementById("image-slot-1").src = itemSRC[items.indexOf(userItems[0])];
+    }
+    else {
+        document.getElementById("image-slot-1").src = "slot.png";
+    }
+    if (userItems[1] != null) {
+        document.getElementById("image-slot-2").src = itemSRC[items.indexOf(userItems[1])];
+    }
+    else {
+        document.getElementById("image-slot-2").src = "slot.png";
+    }
+}
+
+function loseItem(item) {
+    userItems[userItems.indexOf(item)] = null;
+    listItems();
+}
+
+function playAudio(wallId) {
+    switch(wallId) {
+        case 2:
+            document.getElementById("sfx-rain").play();
+            break;
+    }
+}
+
+function pauseAudio(test) {
+    switch(test) {
+        case 2:
+            document.getElementById("sfx-rain").pause();
             break;
     }
 }
